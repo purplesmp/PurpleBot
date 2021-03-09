48db3f399e39c15c93ffab1429c949429f919a30
Oliver Shearman <bygollyitsolli@gmail.com>
2021-03-09 18:01:42 +1100

    added packages, main class and fixed d4j dependency

--- src/main/java/PurpleBot.java
+++ src/main/java/PurpleBot.java
@@ -1,4 +1,27 @@
-
+import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class PurpleBot extends JavaPlugin{
+
+    public static String botToken;
+
+    private static boolean configCreated = false;
+
+    private static JavaPlugin thisPlugin;
+    private static Server thisServer;
+
+
+    @Override
+    public void onEnable(){
+        thisPlugin = this;
+        thisServer = thisPlugin.getServer();
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
 }
